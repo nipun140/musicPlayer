18b8fb867a6a8c211a1ff434c7a075cb58d2670f
nipun <nipungarg.nipun@gmail.com>
2021-06-08 18:59:46 +0530

    changed files

---
+++ index.js
@@ -0,0 +1,72 @@
+let prevBtn = document.getElementById("prev");
+let playBtn = document.getElementById("play");
+let forwBtn = document.getElementById("forward");
+let singerN = document.querySelector("#singer");
+let songN = document.querySelector("#song");
+let music = document.querySelector("audio");
+let img = document.querySelector("img");
+let pauseBtn = document.querySelector(".fa-pause");
+
+prevBtn.addEventListener("click", () => {});
+
+let playMusic = () => {
+  music.play();
+  playBtn.classList.replace("fa-play", "fa-pause");
+  img.classList.add("anim_rotate");
+  isPlaying = true;
+};
+
+let pauseMusic = () => {
+  music.pause();
+  playBtn.classList.replace("fa-pause", "fa-play");
+  img.classList.remove("anim_rotate");
+  isPlaying = false;
+};
+
+let isPlaying = false; //initially false not playing
+playBtn.addEventListener("click", () => {
+  if (!isPlaying) {
+    playMusic();
+  } else {
+    pauseMusic();
+  }
+});
+
+//change songs
+
+let songs = [
+  {
+    name: "song1",
+    singer: "loyalist",
+    song: "lotus lane",
+  },
+  {
+    name: "song2",
+    singer: "aurouro",
+    song: "saphiones",
+  },
+  { name: "song3", singer: "david guetta", song: "hit me back" },
+];
+
+let loadsongs = (song) => {
+  songN.innerHTML = `${song.song}`;
+  singerN.innerHTML = `${song.singer}`;
+  music.src = `music/${song.name}.mp3`;
+  img.src = `img/${song.name}.jpg`;
+};
+
+let songIndex = 0;
+
+forwBtn.addEventListener("click", () => {
+  songIndex++;
+  songIndex = songIndex % songs.length;
+  loadsongs(songs[songIndex]);
+  playMusic(); //play music if changed while playing the previuos music
+});
+
+prevBtn.addEventListener("click", () => {
+  songIndex--;
+  songIndex = (songIndex - 1 + songs.length) % songs.length;
+  loadsongs(songs[songIndex]);
+  playMusic();
+});
